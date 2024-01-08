1105cb66be448fe261d4434ec82d86420d78486c
Adil <adilc0070@gmail.com>
2024-01-08 23:34:45 +0530

    workout

--- DSA week 2/try.js
+++ DSA week 2/try.js
@@ -21,3 +21,18 @@ function merge(left,right){
     }
     return [...sorted,...left,...right]
 }
+
+function buble(arr){
+    let swap
+    do{
+        swap=false
+        for(let  i=0;i<arr.length-1;i++){
+            if(arr[i]<arr[i+1]){
+                let temp=arr[i]
+                arr[i]=arr[i+1]
+                arr[i+1]=temp
+                swap=true
+            }
+        }
+    }while(swap)
+}
