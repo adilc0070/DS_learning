825385d0ab905bb712912fb3e4e951f815e12e7d
Adil <adilc0070@gmail.com>
2024-01-16 22:01:29 +0530

    graph

---
+++ DSA week3/bstT1.js
@@ -0,0 +1,39 @@
+class Node{
+    constructor(value){
+        this.value=value
+        this.left=null
+        this.right=null
+    }
+}
+class BinarySearchTree{
+    constructor(){
+        this.root=null
+    }
+    isEmpty(){
+        return this.root===null
+    }
+    insert(value){
+        let newNode=new Node(value)
+        if(this.isEmpty()){
+            this.root=newNode
+        }else{
+            this.insertValue(newNode,this.root)
+        }
+    }
+    insertValue(newNode,root){
+        if(newNode.value<root.value){
+            if(root.left==null){
+                root.left=newNode
+            }else{
+                this.insertValue(newNode,root.left)
+            }
+        }else{
+            if(root.right==null){
+                root.right=newNode
+            }else{
+                this.insertValue(newNode,root.right)
+            }
+        }
+    }
+    
+}
